6bb12fa67952a0e196251d3ffb4a3f07c1e3a46d
William <vimscientist69@gmail.com>
2025-05-15 08:54:24 +0200

    fix(SwiftSeleniumExampleTests): updated example test to use swift testing instead

--- Example/Tests/SeleniumSwiftExampleTests/SeleniumSwiftExampleTests.swift
+++ Example/Tests/SeleniumSwiftExampleTests/SeleniumSwiftExampleTests.swift
@@ -6,54 +6,15 @@
 // This package is freely distributable under the MIT license.
 // This Package is a modified fork of https://github.com/ashi-psn/SwiftWebDriver.
 
-import class Foundation.Bundle
-import XCTest
+import Testing
 
-public final class SeleniumSwiftExampleTests: XCTestCase {
-    public func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
-        // Mac Catalyst won't have `Process`, but it is supported for executables.
-        #if !targetEnvironment(macCatalyst)
-
-            let fooBinary = productsDirectory.appendingPathComponent("SeleniumSwiftExample")
-
-            let process = Process()
-            process.executableURL = fooBinary
-
-            let pipe = Pipe()
-            process.standardOutput = pipe
-
-            try process.run()
-            process.waitUntilExit()
-
-            let data = pipe.fileHandleForReading.readDataToEndOfFile()
-            let output = String(data: data, encoding: .utf8)
-
-            XCTAssertEqual(output, "Hello, world!\n")
-        #endif
-    }
-
-    /// Returns path to the built products directory.
-    public var productsDirectory: URL {
-        #if os(macOS)
-            for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-                return bundle.bundleURL.deletingLastPathComponent()
-            }
-            fatalError("couldn't find the products directory")
-        #else
-            return Bundle.main.bundleURL
-        #endif
-    }
-
-    deinit {
-        // Cleanup any resources if needed
+@Suite("SeleniumSwiftExampleTests")
+internal class SeleniumSwiftExampleTests {
+    /// A description
+    /// - Parameters:
+    ///
+    @Test("Example Test")
+    public func exampleTest() {
+        #expect(Bool(true), "Example Test Passed")
     }
 }
